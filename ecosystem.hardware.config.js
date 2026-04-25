a20fe4ab9dc57a660e97b058a72c3f4844c1d32b
MrMahirr <yunusemremahir121@gmail.com>
2026-04-25 18:15:18 +0300

    fix: nfc ve led modulü çalışma hatası giderme

--- ecosystem.hardware.config.js
+++ ecosystem.hardware.config.js
@@ -1,36 +1,25 @@
 module.exports = {
   apps: [
     {
-      name: 'zenith-backend',
-      cwd: '/home/mahir/Zenith/backend',
-      script: 'npm',
-      args: 'run start',
-    },
-    {
-      name: 'zenith-frontend',
-      cwd: '/home/mahir/Zenith/frontend',
-      script: 'npm',
-      args: 'run dev -- --host',
-    },
-    {
-      name: 'zenith-camera',
+      name: 'zenith-led',
       cwd: '/home/mahir/Zenith/ai-service',
       script: '/home/mahir/miniforge3/envs/zenith-ai/bin/python',
-      args: 'main.py --service camera',
+      args: 'main.py --service led',
       interpreter: 'none',
       exec_mode: 'fork',
       autorestart: true,
+      restart_delay: 2000,
       kill_timeout: 5000,
-      env: { DISPLAY: ':0' }
     },
     {
-      name: 'zenith-sensor',
+      name: 'zenith-nfc',
       cwd: '/home/mahir/Zenith/ai-service',
       script: '/home/mahir/miniforge3/envs/zenith-ai/bin/python',
-      args: 'main.py --service sensor',
+      args: 'main.py --service nfc',
       interpreter: 'none',
       exec_mode: 'fork',
       autorestart: true,
+      restart_delay: 2000,
       kill_timeout: 5000,
     },
   ],
